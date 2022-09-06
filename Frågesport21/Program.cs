5288bb1b677908bd2d8b88e631e92b7d1fdb23fe
Nicole Åhgren <ahgren.nicole@elev.ga.ntig.se>
2022-09-06 10:16:54 +0200

    blö

--- Frågesport21/Program.cs
+++ Frågesport21/Program.cs
@@ -20,4 +20,25 @@ else {
 Console.WriteLine(" ");
 Console.WriteLine("Fråga 2: Hur många NTI-Skolor finns det i svergie?");
 Console.WriteLine("1. 19st, 2. 24st, 3. 29st");
-F2 = Console.ReadLine("");
+F2 = Console.ReadLine();
+
+if (F2 == "3") {
+    Console.WriteLine("Amazing det var rätt svar:D");
+}
+else {
+    Console.WriteLine("Det var fel:/ Rätt svar var 3");
+}
+
+Console.WriteLine(" ");
+Console.WriteLine("Fråga 3: Vad står NTI för?");
+Console.WriteLine("1. Nordens Teknikerinstitut, 2. Natur-tekniska institutet, 3. Natur och teknikinstitutet");
+F3 = Console.ReadLine();
+
+if (F3 == "1") {
+    Console.WriteLine("Korrekt, bra jobbat!");
+}
+else {
+    Console.WriteLine("Det var feeel:( Rätt svar var 1");
+}
+
+Console.WriteLine ("Tack för att du gjort min frågesport! Hoppas du lärde dig något nytt:)");
